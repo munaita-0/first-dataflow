fc210d2c85670fa794a0dde8b2384f29d477136a
Suzuki Shogo <shogo807@gmail.com>
2018-08-17 19:44:57 +0900

    copied wordCount

---
+++ src/main/java/com/example/WindowedWordCountCopy.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+import com.example.common.ExampleBigQueryTableOptions;
+import com.example.common.ExampleOptions;
+import com.example.common.WriteOneFilePerWindow;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.DefaultValueFactory;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+
